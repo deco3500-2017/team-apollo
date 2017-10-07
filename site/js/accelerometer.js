0d751ea2e7503c78611fca80608b70322d5890ff
Luke Steyn <lukecaan@gmail.com>
2017-10-07 22:07:52 +1000

    Added Accelerometer.js which tracks recent acceleration of device and stores it in a variable representing overall movement values

---
+++ site/js/accelerometer.js
@@ -0,0 +1,24 @@
+if (window.DeviceMotionEvent) {
+  console.log("Motion supported");
+  window.addEventListener('devicemotion', deviceMotionHandler, false);
+} else {
+  console.log("Motion NOT supported");
+};
+
+var moveVal = 0;
+
+function deviceMotionHandler(eventData) {
+  var info, xyz = "[X, Y, Z]";
+
+  // Grab the acceleration from the results
+  var acceleration = eventData.acceleration;
+  info = xyz.replace("X", acceleration.x);
+  info = info.replace("Y", acceleration.y);
+  info = info.replace("Z", acceleration.z);
+
+  var magnitude = Math.sqrt(Math.pow(acceleration.x, 2) + Math.pow(acceleration.y, 2) + Math.pow(acceleration.y, 2));
+
+  moveVal = 0.998 * moveVal + 0.002 * magnitude;
+
+  console.log(moveVal);
+}
